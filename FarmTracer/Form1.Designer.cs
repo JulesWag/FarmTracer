04c458bc0851308d3f61728be897030c1568da82
JulesWag <117723636+JulesWag@users.noreply.github.com>
2023-10-31 16:22:05 +0100

    develop :Innitialisation du projet

--- FarmTracer/Form1.Designer.cs
+++ FarmTracer/Form1.Designer.cs
@@ -28,12 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            lbUser = new Label();
+            lbPassword = new Label();
+            btnConnexion = new Button();
+            btnVisiteur = new Button();
+            SuspendLayout();
+            // 
+            // lbUser
+            // 
+            lbUser.AutoSize = true;
+            lbUser.Location = new Point(12, 88);
+            lbUser.Name = "lbUser";
+            lbUser.Size = new Size(123, 20);
+            lbUser.TabIndex = 0;
+            lbUser.Text = "Nom d'utilisateur";
+            // 
+            // lbPassword
+            // 
+            lbPassword.AutoSize = true;
+            lbPassword.Location = new Point(690, 88);
+            lbPassword.Name = "lbPassword";
+            lbPassword.Size = new Size(98, 20);
+            lbPassword.TabIndex = 1;
+            lbPassword.Text = "Mot de passe";
+            // 
+            // btnConnexion
+            // 
+            btnConnexion.Location = new Point(12, 409);
+            btnConnexion.Name = "btnConnexion";
+            btnConnexion.Size = new Size(94, 29);
+            btnConnexion.TabIndex = 2;
+            btnConnexion.Text = "Connexion";
+            btnConnexion.UseVisualStyleBackColor = true;
+            // 
+            // btnVisiteur
+            // 
+            btnVisiteur.Location = new Point(647, 409);
+            btnVisiteur.Name = "btnVisiteur";
+            btnVisiteur.Size = new Size(141, 29);
+            btnVisiteur.TabIndex = 3;
+            btnVisiteur.Text = "Mode Visiteur";
+            btnVisiteur.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnVisiteur);
+            Controls.Add(btnConnexion);
+            Controls.Add(lbPassword);
+            Controls.Add(lbUser);
+            Name = "Form1";
+            Text = "Connexion FarmTracer";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lbUser;
+        private Label lbPassword;
+        private Button btnConnexion;
+        private Button btnVisiteur;
     }
 }
